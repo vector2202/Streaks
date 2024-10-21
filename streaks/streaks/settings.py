499bacd7d68476601b56bb762fcbf998cddaf1cb
javierl1359 <53274696+javierl1359@users.noreply.github.com>
2024-10-21 17:08:20 -0600

    Update settings.py

--- streaks/streaks/settings.py
+++ streaks/streaks/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirigir después del inicio de sesión
+#LOGIN_REDIRECT_URL = ''
+
+# Redirigir después del cierre de sesión
+LOGOUT_REDIRECT_URL = 'login'
