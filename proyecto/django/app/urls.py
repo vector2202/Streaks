9889c0b0f2e82e4f399f8258d0260641727bd454
javierl1359 <53274696+javierl1359@users.noreply.github.com>
2024-10-21 04:30:35 -0600

    Delete proyecto/django directory

--- proyecto/django/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('create/', views.create_habit, name='create_habit'),
-    path('view/', views.view_habits, name='view_habits'),
-    path('progress/', views.progress_view, name='progress'),
-]
